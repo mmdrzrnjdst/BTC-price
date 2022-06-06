888fe3c3137d63811e659495e9880a8e8a349f89
MMD <63921703+mmdrzrnjdst@users.noreply.github.com>
2022-06-06 16:57:06 +0430

    Create BTC-price.py

---
+++ BTC-price.py
@@ -0,0 +1,16 @@
+import requests 
+
+
+def inform_holder():
+    print('hi there, price is good ')
+
+my_Good_price = 27000
+
+response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
+
+price = float(response.json()['data']['amount'])
+
+print('at this moment, bitcon is %i Dollors' % price)
+
+if (price < my_Good_price):
+    inform_holder()
